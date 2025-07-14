5ec6ba99a8b90afb3800a482d93a411316012b65
Sean Murphy <sean@iamseanmurphy.com>
2025-07-14 04:29:28 +0000

    2.5.6

--- blogus/inc/ansar/customizer-admin/blogus-admin-plugin-install.php
+++ blogus/inc/ansar/customizer-admin/blogus-admin-plugin-install.php
@@ -35,21 +35,21 @@ function blogus_deprecated_hook_admin_notice() {
                             printf( esc_html__('Welcome to %1$s', 'blogus'), esc_html( $theme_info->Name ), esc_html( $theme_info->Version ) ); ?>
                         </h1>
                         
-                        <p><?php esc_html_e("Thank you for choosing blogus theme. To take full advantage of the complete features of the theme click the Starter Sites and Install and Activate the", "blogus");?> <a href="https://wordpress.org/plugins/ansar-import"><?php esc_html_e("Ansar Import", "blogus");?></a> <?php esc_html_e("plugin then use the demo importer and install the Blogus Demo according to your need.", "blogus"); ?></p>
+                        <p><?php esc_html_e("Thank you for choosing Blogus theme. To take full advantage of the complete features of the theme click the Starter Sites and Install and Activate the", "blogus");?> <a href="https://wordpress.org/plugins/ansar-import"><?php esc_html_e("Ansar Import", "blogus");?></a> <?php esc_html_e("plugin then use the demo importer and install the Blogus Demo according to your need.", "blogus"); ?></p>
 
                         <div class="panel-column-6">
                             <div class="blogus-notice-buttons">
-                                <a class="blogus-btn-get-started button button-primary button-hero blogus-button-padding" href="#" data-name="" data-slug=""><span aria-hidden="true" class="dashicons dashicons-images-alt"></span><?php esc_html_e( 'Starter Sites', 'blogus' ) ?></a>
-                                <a class="blogus-btn-get-started-customize button button-primary button-hero blogus-button-padding" href="<?php echo esc_url( admin_url( '/customize.php' ) ); ?>" data-name="" data-slug=""><span aria-hidden="true" class="dashicons dashicons-welcome-widgets-menus"></span><?php esc_html_e( 'Customize Site', 'blogus' ) ?></a>
+                                <a class="blogus-btn-get-started button button-primary button-hero blogus-button-padding" href="#" data-name="" data-slug=""><span aria-hidden="true" class="dashicons dashicons-images-alt"></span><?php esc_html_e( 'Get Started', 'blogus' ) ?></a>
+                                <a class="blogus-btn-get-started-customize button button-secondary button-hero blogus-button-padding" href="<?php echo esc_url( admin_url( '/customize.php' ) ); ?>" data-name="" data-slug=""><span aria-hidden="true" class="dashicons dashicons-welcome-widgets-menus"></span><?php esc_html_e( 'Customize Site', 'blogus' ) ?></a>
                             </div>
                             <div class="blogus-notice-links">
-                                <div class="blogus-documentation blogus-notice-link">
-                                    <span aria-hidden="true" class="dashicons dashicons-list-view"></span>
-                                    <a class="blogus-documentation" href="<?php echo esc_url('https://docs.themeansar.com/docs/blogus-lite/')?>" data-name="" data-slug=""><?php esc_html_e( 'View Documentation', 'blogus' ) ?></a>
-                                </div>
                                 <div class="blogus-demos blogus-notice-link">
                                     <span aria-hidden="true" class="dashicons dashicons-images-alt"></span>
                                     <a class="blogus-demos" href="<?php echo esc_url('https://demos.themeansar.com/blogus-demos')?>" data-name="" data-slug=""><?php esc_html_e( 'View Demos', 'blogus' ) ?></a>
+                                </div>
+                                <div class="blogus-documentation blogus-notice-link">
+                                    <span aria-hidden="true" class="dashicons dashicons-list-view"></span>
+                                    <a class="blogus-documentation" href="<?php echo esc_url('https://docs.themeansar.com/docs/blogus-lite/')?>" data-name="" data-slug=""><?php esc_html_e( 'View Documentation', 'blogus' ) ?></a>
                                 </div>
                                 <div class="blogus-support blogus-notice-link">
                                     <span aria-hidden="true" class="dashicons dashicons-format-chat"></span>
--- blogus/readme.txt
+++ blogus/readme.txt
@@ -4,8 +4,8 @@ Author: Themeansar
 Requires PHP: 7.4
 Requires at least: 6.7
 Tested up to: 6.8
-Stable tag: 2.5.5
-Version: 2.5.5
+Stable tag: 2.5.6
+Version: 2.5.6
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Tags: one-column, two-columns ,right-sidebar, flexible-header, custom-background, custom-header, custom-menu, editor-style, featured-images, footer-widgets,  theme-options, threaded-comments, rtl-language-support, translation-ready, full-width-template, custom-logo, blog, news
@@ -603,3 +603,6 @@ Added Page Title For Woocommerce.
 
 = Version 2.5.5
 1. Fixed Typography Fonts.
+
+= Version 2.5.6
+1. Fixed Notice.
