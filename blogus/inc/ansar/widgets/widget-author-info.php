9108cc59d1470cae17785c211e9653d8eca6c72c
Sean Murphy <sean@iamseanmurphy.com>
2025-09-22 02:46:19 +0000

    2.6.6

--- blogus/inc/ansar/widgets/widget-author-info.php
+++ blogus/inc/ansar/widgets/widget-author-info.php
@@ -20,6 +20,20 @@ class blogus_author_info extends WP_Widget {
 
         extract($args);
 
+        $defaults = array(
+            'title'    => '',
+            'name'     => '',
+            'desc'     => '',
+            'image_uri'=> '',
+            'facebook' => '',
+            'twt'      => '',
+            'insta'    => '',
+            'youtube'  => '',
+            'open_btnone_new_window' => 0,
+        );
+        
+        $instance = wp_parse_args( (array) $instance, $defaults );
+
         echo $before_widget;
         
         $blogus_btnone_target = '_self';
@@ -67,31 +81,43 @@ class blogus_author_info extends WP_Widget {
         <?php echo $after_widget;
 
     }
+
     function update($new_instance, $old_instance) {
-
         $instance = $old_instance;
-        $instance['facebook'] = stripslashes(wp_filter_post_kses($new_instance['facebook']));
-        $instance['open_btnone_new_window'] = strip_tags($new_instance['open_btnone_new_window']);
-        $instance['image_uri'] = strip_tags($new_instance['image_uri']);
-        $instance['title'] = strip_tags($new_instance['title']);
-        $instance['name'] = strip_tags($new_instance['name']);
-        $instance['desc'] = strip_tags($new_instance['desc']);
-        $instance['twt'] = stripslashes(wp_filter_post_kses($new_instance['twt']));
-        $instance['insta'] = stripslashes(wp_filter_post_kses($new_instance['insta']));
-        $instance['youtube'] = stripslashes(wp_filter_post_kses($new_instance['youtube']));
 
-        $businessup_btnone_target = '_self';
-        if( !empty($instance['open_btnone_new_window']) ):
-            $businessup_btnone_target = '_blank';
-        endif;
+        // Basic text fields
+        $instance['title'] = sanitize_text_field( $new_instance['title'] ?? '' );
+        $instance['name']  = sanitize_text_field( $new_instance['name'] ?? '' );
+
+        // Description - lets keep it plain text but use textarea sanitizer
+        $instance['desc'] = sanitize_textarea_field( $new_instance['desc'] ?? '' );
+
+        // Image URL and social URLs — sanitize as URLs
+        $instance['image_uri'] = esc_url_raw( $new_instance['image_uri'] ?? '' );
+        $instance['facebook']  = esc_url_raw( $new_instance['facebook'] ?? '' );
+        $instance['twt']       = esc_url_raw( $new_instance['twt'] ?? '' );
+        $instance['insta']     = esc_url_raw( $new_instance['insta'] ?? '' );
+        $instance['youtube']   = esc_url_raw( $new_instance['youtube'] ?? '' );
+
+        // Checkbox — ensure it's always 0 or 1 (avoids undefined index / null)
+        $instance['open_btnone_new_window'] = ! empty( $new_instance['open_btnone_new_window'] ) ? 1 : 0;
 
         return $instance;
     }
 
     function form($instance) {
-        $instance['title'] = (isset($instance['title'])?$instance['title']:'Author Details');
-        $instance['name'] = (isset($instance['name'])?$instance['name']:'');
-        $instance['desc'] = (isset($instance['desc'])?$instance['desc']:'');
+        $defaults = array(
+            'title' => 'Author Details',
+            'name'  => '',
+            'desc'  => '',
+            'image_uri' => '',
+            'facebook' => '',
+            'twt' => '',
+            'insta' => '',
+            'youtube' => '',
+            'open_btnone_new_window' => 0,
+        );
+        $instance = wp_parse_args( (array) $instance, $defaults );
         ?>
         <p>
           <label for="<?php echo esc_attr($this->get_field_id( 'title' )); ?>"><?php esc_html_e( 'Title','blogus' ); ?></label> 
@@ -114,7 +140,7 @@ class blogus_author_info extends WP_Widget {
         </p>
         <p>
           <label for="<?php echo esc_attr($this->get_field_id( 'desc' )); ?>"><?php esc_html_e( 'Description','blogus' ); ?></label> 
-          <input class="widefat" id="<?php echo esc_attr($this->get_field_id( 'desc' )); ?>" name="<?php echo esc_attr($this->get_field_name( 'desc' )); ?>" type="textarea" value="<?php echo esc_attr( $instance['desc'] ); ?>" />
+          <textarea class="widefat" id="<?php echo esc_attr($this->get_field_id( 'desc' )); ?>" name="<?php echo esc_attr($this->get_field_name( 'desc' )); ?>"><?php echo esc_textarea( $instance['desc'] ); ?></textarea>
         </p>
         <table>
             <tr>
@@ -157,12 +183,16 @@ class blogus_author_info extends WP_Widget {
                     <input type="text" name="<?php echo esc_attr($this->get_field_name('youtube')); ?>" id="<?php echo esc_attr($this->get_field_id('youtube')); ?>" value="<?php if( !empty($instance['youtube']) ): echo esc_attr($instance['youtube']); endif; ?>" class="widefat"/>
                 </td>
             </tr>
-            <tr>
-                <td colspan="2">
-                    <input type="checkbox" name="<?php echo esc_attr($this->get_field_name('open_btnone_new_window')); ?>" id="<?php echo esc_attr($this->get_field_id('open_btnone_new_window')); ?>" <?php if( !empty($instance['open_btnone_new_window']) ): checked( (bool) $instance['open_btnone_new_window'], true ); endif; ?> ><?php esc_html_e( 'Open link in new tab','blogus' ); ?>
-                </td>
-            </tr>
         </table>
+        <p>
+            <label for="<?php echo esc_attr($this->get_field_id('open_btnone_new_window')); ?>">
+                <input type="checkbox" 
+                    id="<?php echo esc_attr($this->get_field_id('open_btnone_new_window')); ?>"
+                    name="<?php echo esc_attr($this->get_field_name('open_btnone_new_window')); ?>"
+                    value="1" <?php checked( $instance['open_btnone_new_window'], 1 ); ?> />
+                <?php esc_html_e( 'Open link in new tab','blogus' ); ?>
+            </label>
+        </p>
         <?php
     }
 }
--- blogus/readme.txt
+++ blogus/readme.txt
@@ -4,8 +4,8 @@ Author: Themeansar
 Requires PHP: 7.4
 Requires at least: 6.7
 Tested up to: 6.8
-Stable tag: 2.6.5
-Version: 2.6.5
+Stable tag: 2.6.6
+Version: 2.6.6
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Tags: one-column, two-columns ,right-sidebar, flexible-header, custom-background, custom-header, custom-menu, editor-style, featured-images, footer-widgets,  theme-options, threaded-comments, rtl-language-support, translation-ready, full-width-template, custom-logo, blog, news
@@ -633,3 +633,6 @@ Added Page Title For Woocommerce.
 
 = Version 2.6.5
 1. Fixed Meta Styling Issues.
+
+= Version 2.6.6
+1. Fixed Author Widget Issues.
